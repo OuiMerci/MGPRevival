453f036f327cba1902d5c0155f4f70f3ec96af85
Ouim <pierremorel.dev@gmail.com>
2018-05-04 17:10:12 +0200

    Anim + movement

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    #region Fields
+    static private InputManager _instance = null;
+
+    private const int MAX_RAYCAST_DISTANCE = 25;
+    private GameManager _gameManager = null;
+    private PlayerBehaviour _player = null;
+    private int _backgroundLayerMask = 0;
+    #endregion Fields
+
+    #region Properties
+    static public InputManager Instance
+    {
+        get { return _instance; }
+    }
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        _player = PlayerBehaviour.Instance;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ApplyMovementInput();
+	}
+
+    void ApplyMovementInput()
+    {
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        bool isDashing = Input.GetButton("Dash");
+        _player.Move(new Vector3(HorizontalInput, 0, 0), isDashing);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    #region fields
+    [SerializeField] private float _speed = 0.0f;
+    [SerializeField] private float _dashingSpeed = 0.0f;
+
+    static private PlayerBehaviour _instance = null;
+    private Animator _anim = null;
+    private SpriteRenderer _spriteRenderer = null;
+    int _runHash = Animator.StringToHash("isRunning");
+    int _dashHash = Animator.StringToHash("isDashing");
+    #endregion
+
+    #region Properties
+    static public PlayerBehaviour Instance
+    {
+        get { return _instance; }
+    }
+    #endregion
+
+    #region Methods
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        _anim = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Update player's position and handle animations.
+    /// </summary>
+    /// <param name="movement">Movement added to the player's position</param>
+    public void Move(Vector3 movement, bool isDashing = false)
+    {
+        if(movement != Vector3.zero)
+        {
+            float actualSpeed = isDashing ? _dashingSpeed : _speed;
+            Vector3 newPos = transform.position + movement * actualSpeed * Time.deltaTime;
+            transform.position = newPos; // Ajouter une fonction "ClampPosition" si nécessaire.
+
+            // Handle animations
+            _anim.SetBool(_runHash, true);
+            _anim.SetBool(_dashHash, isDashing);
+
+            _spriteRenderer.flipX = movement.x < 0 ? true : false;
+        }
+        else
+        {
+            // Handle animations
+            _anim.SetBool(_runHash, false);
+            _anim.SetBool(_dashHash, false);
+
+        }
+    }
+    #endregion
+}
